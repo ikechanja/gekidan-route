334dc05d077680759411b303ea628613e47ca59a
ikechanja <ikejiri.asana@gmail.com>
2023-12-17 22:14:24 +0900

    change cloudflare-pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   nitro: {
-    preset: "cloudflare_pages",
+    preset: 'cloudflare-pages',
   },
   modules: [
     '@nuxt/content',
